885dfc612127609cf832642aaadae9e6634607ad
Abhi <meabhishek1512@gmail.com>
2024-04-07 00:11:52 +0530

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-export default function App() {
+import Navbar from "./components/Navbar"
+
+const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline text-gray-500">
-            Hello world!
-        </h1>
+        <>
+            <Navbar />
+        </>
     )
 }
+
+export default App
